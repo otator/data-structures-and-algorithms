19dbad53fc8699181907486ce807e4a534e119b8
AbdalQader mhemed <abdalqader.mhemed@gmail.com>
2021-04-26 00:09:15 +0300

    add contains and fix problems

--- challenges/codes/app/src/main/utilites/MultiBracketValidation.java
+++ challenges/codes/app/src/main/utilites/MultiBracketValidation.java
@@ -6,6 +6,7 @@ public class MultiBracketValidation {
     Stack stack = new Stack();
     boolean notRemoved;
     for(int i=0; i< input.length(); i++){
+      
       if(input.charAt(i) == '}' && !stack.isEmpty()){
         notRemoved = stack.popUntilValue("{", "(", "[");
       }
